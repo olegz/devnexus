95015f2ceaacc8d6ffe044305ad75817eccb3bb9
Glenn Renfro <grenfro@vmware.com>
2024-04-03 13:52:56 -0400

    Re-add WebTests for recipe-local-web

--- recipe-local-web/src/test/java/org/devnexus/WebTests.java
+++ recipe-local-web/src/test/java/org/devnexus/WebTests.java
@@ -1,21 +1,37 @@
 package org.devnexus;
 
-import java.util.function.Function;
+import java.net.URI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 
-import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = "spring.main.web-application-type=servlet")
 class WebTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private TestRestTemplate resetTemplate;
 
+    /*
+     * This test assumes you have a function of the following signature 'Function<String, String> test()'
+     * which uppercases a String. hte main point of this test is that a function is invoked and tested as an HTTP endpoint
+     * using Spring Cloud Function,
+     * so please modify the test and assertion to fit yoru actual function.
+     */
+    @Test
+    void validateOrderProcessingWeb() throws Exception {
+        ResponseEntity<String> result = this.resetTemplate.exchange(
+                RequestEntity.post(new URI("/uppercase")).body("hello devnexus"), String.class);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        System.out.println(result.getBody());
+        assertThat(result.getBody()).isEqualTo("HELLO DEVNEXUS");
     }
 
 }
