f542dbc2977439debf8e5c3532a777ef3465e3f1
Glenn Renfro <grenfro@vmware.com>
2024-04-03 11:41:22 -0400

    Add test shim for webtests to see if we can use Spring CLI to add

---
+++ recipe-local-web/src/test/java/org/devnexus/WebTests.java
@@ -0,0 +1,21 @@
+package org.devnexus;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@SpringBootTest
+class WebTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
